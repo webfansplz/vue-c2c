fb7bc0ebe05d3e577f0337bcd10c4ae324d1f658
webfansplz <308241863@qq.com>
2023-04-16 00:43:47 +0800

    chore: update playground

---
+++ playground/unocss.config.ts
@@ -0,0 +1,14 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetTypography,
+  presetUno,
+} from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetTypography(),
+  ],
+})
