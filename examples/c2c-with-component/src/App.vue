d6ba8f2de8cde3d492674a7feed654335bbf84c3
webfansplz <308241863@qq.com>
2023-04-16 22:26:29 +0800

    chore: update example

--- examples/c2c-with-component/src/App.vue
+++ examples/c2c-with-component/src/App.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { c2cWithTemplate } from 'vue-c2c'
+import { c2c } from 'vue-c2c'
 import Tooltip from './components/Tooltip.vue'
 
-const useTooltip = c2cWithTemplate(Tooltip)
+const useTooltip = c2c(Tooltip, {
+  withComponent: true,
+})
 
 const props = ref({
   content: 'Hi',
 })
 
-const { show, hide, toggle, visible, template: Placeholder } = useTooltip(props)
+const { show, hide, toggle, visible, VComponent } = useTooltip(props)
 
 // function toggle() {
 //   visible.value ? hide() : show()
@@ -19,7 +21,7 @@ const { show, hide, toggle, visible, template: Placeholder } = useTooltip(props)
 <template>
   <div w-screen h-screen flex items-center justify-center>
     <button relative w-35 h-12 text-6 rounded-2 border-1 cursor-pointer bg-white hover="bg-#f8f8f8" @mouseenter="toggle" @mouseleave="toggle">
-      <Placeholder />
+      <VComponent />
       Hover Me
     </button>
   </div>
